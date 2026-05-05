602b658319edf46d44610c72b26bf566b203420f
Amit Pandey <amitpandeymtr2002@gmail.com>
2026-05-05 17:35:26 +0530

    Add project files.

---
+++ DemoMyWebAPI/CustomMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace DemoMyWebAPI
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _Next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Request recieved...!!!");
+
+            await _Next(context);
+
+            Console.WriteLine("Response Sent...!!");
+
+
+        }
+
+    }
+}
+
